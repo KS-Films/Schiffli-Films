5ebc12e34435078a8db9491f4a39987a523e9fa7
KS-Films <125628973+KS-Films@users.noreply.github.com>
2023-02-16 23:00:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+// function glitch(i=1) {
+//   setTimeout(function() {   
+//     var glitchHeight = 30 + (100 * Math.random());
+//     var glitchOpacity = 1 * Math.random();
+//     document.getElementById("glitch-x").style.height = glitchHeight.toString() + '%';
+//     document.getElementById("glitch-x").style.opacity = glitchOpacity.toString();
+//     document.getElementById("red-back").style.transform = 'perspective(' + glitchHeight  +'em)';
+//     i++;
+//     if (i < 10) {
+//       glitch(i);
+//     }
+//   }, 50)
+// }
+
+// window.setInterval(function(){ 
+//     glitch();
+// }, 5000);
+  
